05999f42a50581ce75257879fc39a3c07918c89c
Matthew_Bowron <matthew.bowron@ucalgary.ca>
2025-02-10 16:50:57 -0700

    Assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+    return (
+      <main>
+        <h1>Matther Bowron</h1>
+        <Link href="https://webdev2-weld.vercel.app/week-2/react">GitHub</Link>
+      </main>
+    );
+  }
