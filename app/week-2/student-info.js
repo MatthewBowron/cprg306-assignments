bcabd36b78df3390a42657cedaf9532391f6a68d
Matthew_Bowron <matthew.bowron@ucalgary.ca>
2025-05-13 10:28:37 -0600

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <h3>Matthew Bowron</h3>
+            <Link href="https://github.com/MatthewBowron/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    );
+}
